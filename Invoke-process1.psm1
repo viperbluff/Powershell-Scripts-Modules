414d23ad7a9cc6050a38f55587059a3d27255228
Sahil Tikoo <pig.wig45@gmail.com>
2020-06-26 17:20:19 +0400

    Added powershell module
    This module initiates process through WMI and Invoke-Command powershell Toolkits

---
+++ Invoke-process1.psm1
@@ -0,0 +1,38 @@
+﻿Function Start-WMIprocess 
+
+{
+
+    [cmdletbinding (supportsShouldprocess=$True)]
+    param (
+    [parameter()]
+    $b
+    )
+
+    Write-Verbose "starting WMI method" 
+   
+    Invoke-WmiMethod -Class Win32_process -Name Create -ArgumentList “$b"
+    
+
+    
+
+}
+
+Function start-anyprocess
+
+{
+
+    
+    [cmdletbinding (supportsShouldprocess=$True)]
+    param(
+    [parameter(Mandatory=$True)]
+
+    $c 
+
+    )
+
+    Write-Verbose "Starting given process"
+    Invoke-Command -ScriptBlock {start-process $c} 
+    
+}
+
+ 
